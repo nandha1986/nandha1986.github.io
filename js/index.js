52d7b6c201aa66aa864bd63ba2369eca2aca98b1
nandha1986 <to.nandhagopal@gmail.com>
2016-03-14 20:14:17 +0530

    Added a tab called projects

--- js/index.js
+++ js/index.js
@@ -1,14 +1,19 @@
 (function() {
     'use strict';
 
-    $('.tabs').on('click', function(e) {
-        var $li = $(e.target);
-        if ($li.is('li')) {
-            $li.siblings().removeClass('active');
-            $li.addClass('active');
-            $('.details').fadeOut('slow').addClass('hide');
-            var data = $li.data('id');
-            $('#' + data).removeClass('hide').fadeIn('slow');
-        }
+    $('.tab').on('click', function(e) {
+        // var $li = $(e.target);
+        // if ($li.is('li')) {
+        //     $li.siblings().removeClass('active');
+        //     $li.addClass('active');
+        //     $('.details').fadeOut('slow').addClass('hide');
+        //     var data = $li.data('id');
+        //     $('#' + data).removeClass('hide').fadeIn('slow');
+        // }
+        $('.tab').removeClass('active');
+        $(this).addClass('active');
+        $('.details').fadeOut('slow').addClass('hide');
+        var data = $(this).data('id');
+        $('#' + data).removeClass('hide').fadeIn('slow');
     });
 })();
